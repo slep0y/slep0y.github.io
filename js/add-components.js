2aff6f2c2d73a4a3a0d2303f421a82597ebcc7e9
slep0y <slep0y@yandex.ru>
2025-01-21 22:49:13 +0300

    Переиграл скрипты и подключение частей

--- js/add-components.js
+++ js/add-components.js
@@ -1,12 +1,20 @@
 function AddComponents(){
     let componentsWrappers = document.querySelectorAll('[data-include]');
+    let scriptBlocks = document.querySelectorAll('[src="js/add-components.js"]');
     componentsWrappers.forEach((element) => {
             fetch(element.dataset['include'])
             .then(response => {
                 return response.text();
             })
             .then(data => {
-                    element.insertAdjacentHTML('beforeend', data);
+                element.insertAdjacentHTML('beforeend', data);
+                element.removeAttribute('data-include');
+
+                scriptBlocks.forEach((el)=>{
+                    el.remove();
+                });
+
+                AddComponents = undefined;
             })
     });
 }
