ea0dcb908c7753950264cd19dd92fed183c45259
slep0y <slep0y@yandex.ru>
2025-01-23 08:47:55 +0300

    Добавлены страницы: Под рукой и Избранное

--- js/search.js
+++ js/search.js
@@ -1,25 +1,26 @@
 // ! YANDEX-SEARCH ************************
-let searchInput = document.querySelector('.yandex-search');
-let searchInSite = document.querySelector('.search-in-site');
+if(document.querySelector('.search-block')){
+    let searchInput = document.querySelector('.yandex-search');
+    let searchInSite = document.querySelector('.search-in-site');
+    let searchButton = document.querySelector('.yandex-search-button');
 
-let searchButton = document.querySelector('.yandex-search-button');
+    searchInput.onkeyup = searchCheck;
+    searchButton.onclick = searchOnButtonClick;
 
-searchInput.onkeyup = searchCheck;
-searchButton.onclick = searchOnButtonClick;
-
-function searchCheck(ev){
-    if(ev.key == 'Enter'){
-        let url = 'https://yandex.ru/search/?text=' + searchInput.value + ' host:' + searchInSite.value;
-        window.open(url, '_blank');
-        searchInput.value = '';
+    function searchCheck(ev){
+        if(ev.key == 'Enter'){
+            let url = 'https://yandex.ru/search/?text=' + searchInput.value + ' host:' + searchInSite.value;
+            window.open(url, '_blank');
+            searchInput.value = '';
+        }
     }
-}
-function searchOnButtonClick(){
-    if(searchInput.value != ''){
-        let url = 'https://yandex.ru/search/?text=' + searchInput.value + ' host:' + searchInSite.value;
-        window.open(url, '_blank');
-        searchInput.value = '';
-    }else{
-        alert('Введите запрос');
+    function searchOnButtonClick(){
+        if(searchInput.value != ''){
+            let url = 'https://yandex.ru/search/?text=' + searchInput.value + ' host:' + searchInSite.value;
+            window.open(url, '_blank');
+            searchInput.value = '';
+        }else{
+            alert('Введите запрос');
+        }
     }
 }
